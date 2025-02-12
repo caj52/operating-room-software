8e94b073616a895943912088688131aee87ccf19
mudab129 <156838531+mudab129@users.noreply.github.com>
2025-02-12 09:17:29 -0800

    linking new folder

--- Assets/Scripts/MathUtilities/Constraints/Constraints/ConstrainedJoint.cs
+++
@@ -1,4 +0,0 @@
-﻿using UnityEngine;
-public abstract class ConstrainedJoint : MonoBehaviour {
-  public abstract void Evaluate();
-}
--- Assets/Scripts/MathUtilities/VectorField/ParticleModifier.cs
+++
@@ -1,4 +0,0 @@
-﻿using UnityEngine;
-public abstract class ParticleModifier : MonoBehaviour {
-  public abstract void ModifierOffset(int index, ref Vector3 particlePosition, ref Vector3 previousParticlePosition, bool reset = false);
-}
