6c72790fa25063116568b3b255c6f76feab44458
Connor J <80863542+caj52@users.noreply.github.com>
2026-06-29 05:25:09 -0700

    Update door clearance line scale only when root scale changes.

--- Assets/Scripts/DoorClearanceLineScaler.cs
+++ Assets/Scripts/DoorClearanceLineScaler.cs
@@ -1,21 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorClearanceLineScaler : MonoBehaviour
 {
     private float _originalLocalScaleY;
+    private Transform _root;
+    private float _lastRootScaleX;
 
     private void Awake()
     {
         _originalLocalScaleY = transform.localScale.y;
+        _root = transform.root;
+        _lastRootScaleX = _root.localScale.x;
+        ApplyScale();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        float rootScaleX = _root.localScale.x;
+        if (Mathf.Approximately(rootScaleX, _lastRootScaleX))
+            return;
+
+        _lastRootScaleX = rootScaleX;
+        ApplyScale();
+    }
+
+    private void ApplyScale()
     {
         Vector3 scale = transform.localScale;
-        scale.y = transform.root.localScale.x * _originalLocalScaleY;
+        scale.y = _root.localScale.x * _originalLocalScaleY;
         transform.localScale = scale;
     }
 }
